048d05b17a7ccab9cb4aaeb74f1da0ed54339b39
kiki <muhammad.210170181@mhs.unimal.ac.id>
2023-10-10 21:36:02 +0700

    membuat while dan do while loop

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 for (i=1; i<=10; i++) {
     console.log("angka ke-" + i)
 }
+
+j = 1;
+while (j <= 10) {
+    console.log("j = " + j);
+}
+
+k = 1;
+do {
+    console.log("k = " + k);
+    k++;
+} while (k <= 10);
